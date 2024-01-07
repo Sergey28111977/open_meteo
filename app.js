029d182d5f3e7f7ff8fa4afac128cf99674c00b7
Sergey28111977 <sergey.puzankov.slow1@gmail.com>
2024-01-07 15:14:06 +0100

    adding News news api

--- app.js
+++ app.js
@@ -157,6 +157,10 @@ const App = {
             const url = 'https://newsapi.org/v2/top-headlines?country=ua&apiKey=1799f58a5ad44db6b75eec57e4ea5b4f';
             this.commits = await (await fetch(url)).json();           
         },
+
+        formatDate(v) {
+            return v.replace(/T|Z/g, ' ')
+          }
     },
     
     computed: {
